c0f12bdbbfb50b18563a0b199dc2922d5259820a
alecristia <alecristia@gmail.com>
2022-01-14 16:06:39 +0100

    adding childesr data in progress

--- metrics_childesr.R
+++ metrics_childesr.R
@@ -3,12 +3,6 @@ library(tidyverse)
 library(koRpus)
 library(koRpus.lang.fr)
 
-############################ variables
-
-#lang="fra"
-#included_corpora=
-#dan, eng, deu, ita, ell, nor, por, rus, spa, swe, tur, hrv, ces, zho, yue, heb, kor, cat, nld, hun, ind, ron, tam, afr, ara, eus, isl, gle, pol, srp, cym, est, gla, glg, haw, jam, jpn, jav, kik, xmm, mar, nan, esk, fas, que, slv, sot, tgl, taq, tha, und, yuw, yid
-corpus_args <- list(corpus = included_corpora, token = "*")
 
 ############################ metric functions
 
@@ -66,8 +60,15 @@ compute_mattr <- function(metric_data) {
 }
 
 ########################### main functions
+get_childes_data <- function( corpus_args) { #gets data from childes
+  utterances <- get_utterances(corpus = corpus_args$corpus)
+  tokens <- get_tokens(
+                       corpus = corpus_args$corpus,
+                       token = corpus_args$token)
+  return(list("utterances" = utterances, "tokens" = tokens))
+}
 
-get_childes_data <- function(childes_lang ="fra", corpus_args) { #gets data from childes
+get_childes_data_lang <- function(childes_lang ="fra", corpus_args) { #gets data from childes
   utterances <- get_utterances(language = childes_lang,
                                corpus = corpus_args$corpus)
   tokens <- get_tokens(language = childes_lang,
@@ -102,8 +103,18 @@ get_childes_metrics <- function(childes_data) { #computes metrics on childes dat
   return(metric_tokens |>
            full_join(metric_utts))
 }
+############################ variables
+
+#lang="fra"
+#included_corpora=
+#dan, eng, deu, ita, ell, nor, por, rus, spa, swe, tur, hrv, ces, zho, yue, heb, kor, cat, nld, hun, ind, ron, tam, afr, ara, eus, isl, gle, pol, srp, cym, est, gla, glg, haw, jam, jpn, jav, kik, xmm, mar, nan, esk, fas, que, slv, sot, tgl, taq, tha, und, yuw, yid
+corpus_args <- list(token = "*") #corpus = included_corpora, 
+
 
 ###############################
+
+
+
 
 transcripts <- get_transcripts()
 
---
+++ wdi-lookup.R
@@ -0,0 +1,24 @@
+library(WDI)
+
+# Indices
+# WDIsearch('gdp')
+#"GDP per capita, PPP (constant 2005 international $)"
+# WDIsearch('Educational attainment, at least')
+# Naomi: secondary, some college, university, 
+# WDI:  SE.SEC.CUAT.LO.ZS    "Educational attainment, at least completed lower secondary, population 25+, total (%) (cumulative)" -- 62 countries are NOT NA
+# WDI:  SE.SEC.CUAT.UP.ZS   Educational attainment, at least completed upper secondary, population 25+, total (%) (cumulative) -- 66 countries are NOT NA
+# WDI:  SE.TER.CUAT.BA.ZS   Educational attainment, at least Bachelor's or equivalent, population 25+, total (%) (cumulative) -- only 25 countries are not NA
+# note, only 23 countries have all three... 
+
+## ADD
+# "SH.FPL.IDLC.Q3" "Mean ideal number of children (per woman): Q3"
+# "SP.DYN.CEBN.Q3" "Mean number of children ever born to women aged 40-49: Q3" 
+
+
+dat_all = WDI(indicator=c('NY.GDP.PCAP.PP.KD','SE.SEC.CUAT.UP.ZS','SP.RUR.TOTL.ZS','5.01.01.01.indust','SP.DYN.CEBN.Q3'), start=2012, end=2012)
+
+# flip %rur
+dat_all$SP.URB.TOTL.ZS=100-dat_all$SP.RUR.TOTL.ZS
+
+write.csv(dat_all,"wdi-data.csv")
+
